e92352bf3bcf351c833b611c06c7f8b368383e16
jhuancapaza <jhuancapaza@DH61CR1212-48.loctech.com>
2013-11-20 20:21:22 -0500

    Se añadio Facelets.

--- src/main/java/pe/edu/cibertec/proyectocurso/managedbean/CursoManagedBean.java
+++ src/main/java/pe/edu/cibertec/proyectocurso/managedbean/CursoManagedBean.java
@@ -7,12 +7,14 @@ package pe.edu.cibertec.proyectocurso.managedbean;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.faces.model.SelectItem;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import pe.edu.cibertec.proyectocurso.model.Curso;
 import pe.edu.cibertec.proyectocurso.service.CursoService;
+import pe.edu.cibertec.proyectocurso.util.Constante;
 
 /**
  *
@@ -23,22 +25,19 @@ import pe.edu.cibertec.proyectocurso.service.CursoService;
 @Scope("session")
 public class CursoManagedBean implements Serializable{
     private static Logger logger = Logger.getLogger(CursoManagedBean.class);
-
-    /**
-     * Creates a new instance of CursoManagedBean
-     */
     
     //@ManagedProperty(value="#{cursoService}")
     @Autowired
     private CursoService servicioCurso;
     private List<Curso> listaCursos= new ArrayList<Curso>();
     
-    private Curso objCurso= new Curso();
-    
+    private Curso objCurso= new Curso();    
     private Integer codigoCurso;
+    private List<SelectItem> listTipoCurso;
     
     public CursoManagedBean() {        
         logger.info("Inicio - CursoManagedBean");
+        cargar();
         logger.debug("Fin - CursoManagedBean");
     }
     
@@ -49,15 +48,13 @@ public class CursoManagedBean implements Serializable{
         
         return "/curso/listaCursos?faces-redirect=true";
     }
-
-    
-    
+        
     public String grabarCurso(){
         if(objCurso.getCodigoCurso()==-1){
-             System.out.print("ENTRA A INSERTAR CURSOS");
+             logger.info("ENTRA A INSERTAR CURSOS");
              servicioCurso.grabarCurso(objCurso);
         }else{
-             System.out.print("ENTRA A ACTUALIZAR CURSOS");
+             logger.info("ENTRA A ACTUALIZAR CURSOS");
              servicioCurso.actualizarCurso(objCurso);
         }
        
@@ -87,7 +84,16 @@ public class CursoManagedBean implements Serializable{
         return listarCursos();
         
     }
-
+    
+    //Otros Metodos
+    private void cargar(){
+        listTipoCurso = new ArrayList<SelectItem>();
+        listTipoCurso.add(new SelectItem(Constante.TIPOCURSO_CIENCIAS, Constante.TIPOCURSO_CIENCIAS_DESC));
+        listTipoCurso.add(new SelectItem(Constante.TIPOCURSO_HUMANIDADES, Constante.TIPOCURSO_HUMANIDADES_DESC));
+        listTipoCurso.add(new SelectItem(Constante.TIPOCURSO_POLITICA, Constante.TIPOCURSO_POLITICA_DESC));
+    }
+    
+    
     //Metodos Get - Set
     public Integer getCodigoCurso() {
         return codigoCurso;
@@ -104,7 +110,6 @@ public class CursoManagedBean implements Serializable{
     public void setServicioCurso(CursoService servicioCurso) {
         this.servicioCurso = servicioCurso;
     }
-
    
     public List<Curso> getListaCursos() {
         return listaCursos;
@@ -120,6 +125,10 @@ public class CursoManagedBean implements Serializable{
 
     public void setObjCurso(Curso objCurso) {
         this.objCurso = objCurso;
+    }
+
+    public List<SelectItem> getListTipoCurso() {
+        return listTipoCurso;
     }
     
 }
---
+++ src/main/java/pe/edu/cibertec/proyectocurso/util/Constante.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.cibertec.proyectocurso.util;
+
+/**
+ *
+ * @author jhuancapaza
+ */
+public interface Constante {
+    public static final String TIPOCURSO_CIENCIAS    	= "C";
+    public static final String TIPOCURSO_CIENCIAS_DESC   = "CIENCIAS";
+    public static final String TIPOCURSO_HUMANIDADES    	= "H";
+    public static final String TIPOCURSO_HUMANIDADES_DESC   = "HUMANIDADES";
+    public static final String TIPOCURSO_POLITICA    	= "P";
+    public static final String TIPOCURSO_POLITICA_DESC   = "POLITICA";
+}
